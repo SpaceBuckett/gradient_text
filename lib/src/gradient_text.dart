f08f0d0e345575c2a0175fc81969a8746f6491b9
echo-shahzad <shehxadv2.0@gmail.com>
2025-03-01 15:20:18 +0500

    Initial commit

---
+++ lib/src/gradient_text.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GradientText extends StatelessWidget {
+  final String text;
+  final TextStyle style;
+  final List<Color> gradientColors;
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final TextAlign textAlign;
+
+  const GradientText({
+    super.key,
+    required this.text,
+    required this.style,
+    required this.gradientColors,
+    this.begin = Alignment.topCenter,
+    this.end = Alignment.bottomCenter,
+    this.textAlign = TextAlign.left,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback:
+          (bounds) => LinearGradient(
+            begin: begin,
+            end: end,
+            colors: gradientColors,
+          ).createShader(bounds),
+      child: Text(text, style: style, textAlign: textAlign),
+    );
+  }
+}
+
+
